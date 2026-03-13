b2ad4f2434c877eb78e8e7dc06fae04fa8b88697
lofeodo <daniel.lofeodo@gmail.com>
2026-03-13 14:24:11 -0400

    create .env file

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ from __future__ import annotations
 import os
 from dataclasses import dataclass
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 @dataclass(frozen=True)
